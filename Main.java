67696a68122914b4ba44016cd717c0f0980d610e
ReltonTolpo <ori.taylor@gmail.com>
2016-01-28 19:45:58 +0000

    Adding buttons!
    YAAAAAAAAAY

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 package pkg2d.window.game;
 
+import java.awt.FlowLayout;
 import javax.swing.JFrame;
+import javax.swing.JButton;
 
 public class Main extends JFrame{
 
@@ -17,7 +19,12 @@ public class Main extends JFrame{
         setSize(1024, 768); //800 x 600
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
         //;
+        JButton begin = new JButton("Click Me!");
+        add(begin);
+        JButton test = new JButton("I'm a test");
+        add(test);
     }
     
 }
